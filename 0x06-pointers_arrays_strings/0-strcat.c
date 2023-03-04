6bc91495948f4f300bea0b8037bd2acb655666df
MbillaFactor1 <mbillaifekwe@gmail.com>
2023-03-03 22:43:07 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+using *_strcat to concatenates two strings
+char *dest, 
+chat *src/
+
+char *-strcat(char *dest, char *src)
+{
+        int i;
+        int i;
+
+        i = 0;
+        whie (dest[i] != '\')
+	{
+               i++;
+        }
+        j = 0;
+        while (src[j] != '\0')
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+
+        dest[i] = '\0';
+        return (dest);
+}
